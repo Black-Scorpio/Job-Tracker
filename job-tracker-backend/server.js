b9f1e2e0a161734feba28f213bd4c76eabcb6254
Black-Scorpio <ScorpioGentles@outlook.com>
2024-05-25 08:07:33 -0400

    create backend with use of docker for mongodb

---
+++ job-tracker-backend/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+mongoose
+  .connect("mongodb://localhost:27017/jobtracker")
+  .then(() => console.log("MongoDB connected..."))
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
